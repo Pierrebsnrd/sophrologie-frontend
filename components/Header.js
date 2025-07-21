13137e25c2ac06cf65f660a71952c3fef8a98e11
pierre <pierre.boisnard@live.fr>
2025-07-21 16:47:41 +0200

    fix logo

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Logo from '../public/logo.png';
 
 export default function Header() {
   return (
     <nav className="w-full bg-white shadow-md py-4 px-6 flex items-center justify-between">
       <div className="flex items-center space-x-4">
-        <Image src={Logo} alt="Logo" width={50} height={50} />
+        <Image src="logo.png" alt="Logo" width={50} height={50} />
         <h1 className="text-xl font-semibold text-gray-800">Cabinet de Sophrologie</h1>
       </div>
       <ul className="flex space-x-6 text-gray-700 font-medium">
