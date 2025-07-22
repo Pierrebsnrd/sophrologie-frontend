2c2c921beff49781596436c597905785417446b1
pierre <pierre.boisnard@live.fr>
2025-07-22 17:20:37 +0200

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.sophrologuevillepreux.fr/',
+  siteUrl: 'https://www.sophrologuevillepreux.fr/', // remplace par ton vrai domaine
   generateRobotsTxt: true,
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ['/admin'], // optionnel : pages à ne pas indexer
 }
