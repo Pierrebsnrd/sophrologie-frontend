8b3333b0231bbcc308490d4a1f0df3b18f71b8e1
pierre <pierre.boisnard@live.fr>
2025-07-24 18:10:10 +0200

    update style and meta

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,8 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Stéphanie Habert Sophrologue</title>
-        <meta name="description" content="Prenez rendez-vous facilement avec notre cabinet de sophrologie" />
+        <title>Stéphanie Habert – Sophrologie à Villepreux</title>
+        <meta name="description" content="Consultation de sophrologie personnalisée à Villepreux avec Stéphanie Habert. Prenez rendez-vous facilement pour améliorer votre bien-être." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon.png" />
         <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
