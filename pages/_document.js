0dad8702fc5eb1b3b987c054222b9d455cf4a9c2
pierre <pierre.boisnard@live.fr>
2025-07-24 22:53:40 +0200

    _document.js

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Favicon commun */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon.png" />
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
