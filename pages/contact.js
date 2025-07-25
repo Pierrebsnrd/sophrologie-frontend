077f54d9b61293a1bacf098754b8103b93c579a3
pierre <pierre.boisnard@live.fr>
2025-07-25 12:01:18 +0200

    try to fix h1 error

--- pages/contact.js
+++ pages/contact.js
@@ -107,19 +107,19 @@ export default function Contact() {
                     </section>
                     <div className={styles.contentWrapper}>
                         <section className={styles.contactSection}>
-                            <h1>Contact</h1>
+                            <h2 className={styles.mainTitle}>Coordonnées</h2>
                             <p><strong>Cabinet :</strong> Stéphanie Habert Sophrologue</p>
                             <p><strong>Adresse :</strong> Villepreux, 78450</p>
-                            <h2>Horaires d'ouverture</h2>
+                            <h3>Horaires d'ouverture</h3>
                             <ul>
                                 <li>Lundi au vendredi : 9h - 18h</li>
                                 <li>Samedi : 9h - 12h</li>
                                 <li>Dimanche : Fermé</li>
                             </ul>
-                            <h2>Contact</h2>
+                            <h3>Contact</h3>
                             <p><strong>Téléphone :</strong> <a href="tel:0611421765">06 11 42 17 65</a></p>
                             <p><strong>Email :</strong> <a href="mailto:stephaniehabert.sophrologue@gmail.com">stephaniehabert.sophrologue@gmail.com</a></p>
-                            <h2>Réseaux sociaux</h2>
+                            <h3>Réseaux sociaux</h3>
                             <div className={styles.socialIcons}>
                                 <a href="https://www.facebook.com/share/1BnUXyDqhg/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                                     <FaFacebookF />
