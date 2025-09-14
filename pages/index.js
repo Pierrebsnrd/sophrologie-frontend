b387b12db67b45f8e6c86c6d907a9d2f74788c9e
pierre <pierre.boisnard@live.fr>
2025-09-14 18:25:49 +0200

    update keywords for SEO

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ function Index() {
         canonical="https://www.sophrologuevillepreux.fr/"
         ogImage="https://www.sophrologuevillepreux.fr/bannieres/accueil.jpg"
         pageType="website"
-        keywords="sophrologie Villepreux, Stéphanie Habert, bien-être, relaxation, gestion stress"
+        keywords="sophrologue Villepreux, sophrologie Villepreux, sophrologue Saint-Germain-en-Laye, sophrologie Saint-Germain-en-Laye, relaxation, gestion du stress, bien-être, Stéphanie Habert, séances personnalisées"
       />
       <Home />
     </>
